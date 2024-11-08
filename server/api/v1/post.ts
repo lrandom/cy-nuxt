435924c93c458cd4fa91d19ef16ebb9bf52f9fb8
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-11-08 11:33:42 +0700

    api

--- server/api/v1/post.ts
+++ server/api/v1/post.ts
@@ -1,7 +1,15 @@
+import {faker} from '@faker-js/faker';
+
 export default defineEventHandler(async (event) => {
-    return {
-        data: {
-            message: "Hello World"
+    //return for me a list of product data with 10 items using faker
+    return Array.from({length: 10}, () => {
+        return {
+            id: faker.string.uuid(),
+            name: faker.commerce.productName(),
+            price: faker.commerce.price(),
+            description: faker.commerce.productDescription(),
+            image: faker.image.urlPlaceholder()
         }
-    }
+    })
+
 });
